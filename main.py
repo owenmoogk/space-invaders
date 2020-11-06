22424ec502979cf1f09e0966a88d124f2e11b2a6
odoggy21 <owenmoogk@gmail.com>
2020-11-06 16:58:22 -0500

    enemy moves

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 import pygame
+import random
 
 #intalize
 pygame.init()
 
+#settings
+screenX = 800
+screenY = 600
+playerSize = 64 #pixels
+playerSpeed = 0.3
+
 #create screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((screenX,screenY))
 
 #title and icon
 pygame.display.set_caption("Space Invaders")
@@ -15,19 +22,19 @@ pygame.display.set_icon(icon)
 
 #player
 playerImg = pygame.image.load('img/player.png')
-playerSize = 64 #player size in pixels, used for aligning with walls and center
-playerX = 400-playerSize/2
-playerY = 480
-
+playerX = (screenX/2)-playerSize/2
+playerY = screenY - (screenY/5)
 def player(x,y):
     screen.blit(playerImg,(x,y))
 
 #enemy
 enemyImg = pygame.image.load('img/enemy.png')
 enemySize = 64 #player size in pixels, used for aligning with walls and center
-enemyX = 400-enemySize/2
+enemyX = random.randint(0,800-enemySize)
 enemyY = 70
-
+enemySpeedX = 0.3
+enemyDirection = bool(random.randint(0,1))
+    
 def enemy(x,y):
     screen.blit(enemyImg,(x,y))
 
@@ -49,14 +56,13 @@ while True:
     player(playerX,playerY)
     enemy(enemyX,enemyY)
 
-    speed = 0.3 #movement speed
-
+    #player movement
     if event.type == pygame.KEYDOWN:
 
         if event.key == pygame.K_LEFT:
-            playerX -= speed
+            playerX -= playerSpeed
         if event.key == pygame.K_RIGHT:
-            playerX += speed
+            playerX += playerSpeed
         if playerX < 0:
             playerX = 0
         if playerX > 800-playerSize:
@@ -71,7 +77,25 @@ while True:
         # if y > 600-playerSize:
         #     y = 600-playerSize
 
+    #enemy movement
+    
 
+
+    
+    if enemyDirection: #right
+        enemyX += enemySpeedX
+    else:
+        enemyX -= enemySpeedX
+
+
+    if 0 > enemyX or enemyX > screenX-enemySize:
+        enemyDirection = not enemyDirection
+        enemyY += 30
+        if enemyY < 100:
+            pygame.quit()
+            sys.exit()
+    
+    
 
     #needed to update the screen
     pygame.display.update()
